475634f2b071aa8855f7afe127252058c71429e8
diego-urgell <diegourgell@outlook.com>
2021-09-07 11:18:39 -0500

    Initial Commit

---
+++ cetac-nd-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cetac-nd-app
+//
+//  Created by Diego Urgell on 07/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
